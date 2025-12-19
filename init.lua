83320faa0d8ff8fd403e069c6ab96472b5466033
shimakan <72994273+shimakan@users.noreply.github.com>
2025-12-19 14:37:41 -0800

    add init.lua from source repo

--- init.lua
+++ init.lua
@@ -940,6 +940,7 @@ require('lazy').setup({
   },
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    branch = 'master', -- adding this line fixed the "Failed to run `config` for nvim-treesitter"
     build = ':TSUpdate',
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
